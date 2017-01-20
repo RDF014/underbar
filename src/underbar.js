4cb8b9f9a709a79f088e1f844732caec64b4be83
Raphael Feliciano <rdf014@gmail.com>
2017-01-19 16:47:14 -0800

    fixed delay code.

--- src/underbar.js
+++ src/underbar.js
@@ -331,12 +331,11 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
-    // var args = []
-    var args = Array.prototype.slice.call(arguments[2])
+    var args = []
     
-    // for(var i = 2; i < arguments.length; i++){
-    //   args.push(arguments[i])
-    // }
+    for(var i = 2; i < arguments.length; i++){
+      args.push(arguments[i])
+    }
     
     return setTimeout( function(){
       func.apply(null, args);
